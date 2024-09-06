7020c95580680607b2864b01374d017b02529161
Muhammad Hanzala Khaliq <hanzallahkhaliq@gmail.com>
2024-09-06 20:28:03 +0500

    Updated Name

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -43,7 +43,7 @@ const Navbar = () => {
               animate={{ opacity: 1, scale: 1 }}
               transition={{ duration: 0.5 }}
             >
-              Hanzala Portfolio
+              Portfolio
             </motion.span>
             <div className="social">
               <a
